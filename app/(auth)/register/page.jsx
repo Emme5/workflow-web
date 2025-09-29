745e1ceef8c2a1ae25b05db754833196d1e0e5ef
Emme5 <103664962+Emme5@users.noreply.github.com>
2025-09-29 16:22:37 +0700

    add beta form

--- app/(auth)/register/page.jsx
+++ app/(auth)/register/page.jsx
@@ -1,23 +1,26 @@
 import SignupForm from '@/components/Auth/SignupForm'
 import { ModeToggle } from '@/components/ThemeSwitcher'
-import { Card, CardHeader, CardTitle } from '@/components/ui/card'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import React from 'react'
 
 const SignupPage = () => {
   return (
     <section className='flex items-center justify-center h-screen'>
-     <div className='fixed top-5 right-5'>
-      <ModeToggle />
-     </div>
+      <div className='fixed top-5 right-5'>
+        <ModeToggle />
+      </div>
 
-    <Card className='w-full rounded-lg shadow border border-input md:mt-8 sm:max-w-[32rem] xl:p=0'>
-      <CardHeader>
-        <CardTitle>
-          create an account
-        </CardTitle>
-      </CardHeader>
-
-    </Card>
+      <Card className='w-full rounded-lg shadow border border-input md:mt-8 sm:max-w-[32rem] xl:p=0'>
+        <CardHeader>
+          <CardTitle>
+            create an account
+          </CardTitle>
+          <CardDescription>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, dignissimos!</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <SignupForm />
+        </CardContent>
+      </Card>
 
       <SignupForm />
     </section>
